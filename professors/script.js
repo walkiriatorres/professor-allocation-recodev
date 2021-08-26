72d0cb6ad0d7e3732bac34767745f55af7690c87
walkiriatorres <85717908+walkiriatorres@users.noreply.github.com>
2021-08-26 13:31:08 -0300

    Adicionando validação de CPF com alert
    Validação apenas emite alert, avisando ao usuário que o dado digitado tem erro. Não o impede de salvar.

--- professors/script.js
+++ professors/script.js
@@ -160,3 +160,55 @@ async function loadSelectDepartmentId() {
 loadSelectDepartmentId();
 
 loadTable();
+
+function validarCPF(){
+   var cpf = document.getElementById("txtCPF").value;
+
+   if(cpf.length != 11) {
+   	alert("CPF inválido. Digite CPF com 11 dígitos");
+      return false;
+   }
+      
+   if(cpf == "00000000000" || cpf == "11111111111" ||
+      cpf == "22222222222" || cpf == "33333333333" || 
+      cpf == "44444444444" || cpf == "55555555555" ||
+      cpf == "66666666666" || cpf == "77777777777" ||
+      cpf == "88888888888" || cpf == "99999999999"){
+      alert("CPF inválido. Tente novamente.");
+      return false;
+   }
+  
+   soma = 0;
+   for(i = 0; i < 9; i++){
+     soma += parseInt(cpf.charAt(i)) * (10 - i);
+   }   
+ 
+   resto = 11 - (soma % 11);
+    
+   if(resto == 10 || resto == 11){
+     resto = 0;
+   }
+    
+   if(resto != parseInt(cpf.charAt(9))){
+     alert("CPF inválido. Tente novamente.");
+     return false;
+   }
+ 
+   soma = 0;
+   for(i = 0; i < 10; i ++){
+     soma += parseInt(cpf.charAt(i)) * (11 - i);
+   }   
+ 
+   resto = 11 - (soma % 11);
+   if(resto == 10 || resto == 11){
+     resto = 0;
+   }
+ 
+   if(resto != parseInt(cpf.charAt(10))){
+     alert("CPF inválido. Tente novamente.");
+     return false;
+   }
+ 
+   alert("CPF válido. Muito obrigado."); 
+   return true;
+}
