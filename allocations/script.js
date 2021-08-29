05f9c22b50709e00b8e77dea261aac56221485b7
walkiriatorres <85717908+walkiriatorres@users.noreply.github.com>
2021-08-29 01:17:06 -0300

    removendo sortTable

--- allocations/script.js
+++ allocations/script.js
@@ -55,7 +55,6 @@ async function createLine(allocation) {
 	linha.appendChild(colunaDelete);
 
 	table.insertBefore(linha, table.firstChild);
-	sortTable(table,'asc',0);
 }
 
 async function refreshTable() {
@@ -277,39 +276,3 @@ loadSelectDayHour();
 
 loadTable();
 
-function sortTable(table, dir, n) {
-  var rows, switching, i, x, y, shouldSwitch, switchcount = 0;
-  switching = true;
-  
-  while (switching) {
-    
-    switching = false;
-    rows = table.rows;    
-    for (i = 1; i < (rows.length - 1); i++) {      
-      shouldSwitch = false;      
-      x = rows[i].getElementsByTagName("TD")[n];
-      y = rows[i + 1].getElementsByTagName("TD")[n];
-      if (dir == "asc") {
-        if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
-          shouldSwitch = true;
-          break;
-        }
-      } else if (dir == "desc") {
-        if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
-          shouldSwitch = true;
-          break;
-        }
-      }
-    }
-    if (shouldSwitch) {
-      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
-      switching = true;
-      switchcount++;
-    } else {
-      if (switchcount == 0 && dir == "asc") {
-        dir = "desc";
-        switching = true;
-      }
-    }
-  }
-}
