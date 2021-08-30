d7a38dafc031273b8f7c5b468e5275a466374af8
walkiriatorres <85717908+walkiriatorres@users.noreply.github.com>
2021-08-29 21:20:42 -0300

    Novo elemento em primeiro, demais ordenados

--- courses/script.js
+++ courses/script.js
@@ -34,7 +34,8 @@ async function createLine(cour) {
 	linha.appendChild(colunaDelete);
 
 	const table = document.getElementById("tableBody");
-	table.appendChild(linha);
+	table.insertBefore(linha, table.firstChild);	
+	sortTable(table,'asc',0);
 }
 
 async function refreshTable() {
@@ -125,3 +126,40 @@ const confirmDelete = document.getElementById("btnModalDelete");
 confirmDelete.addEventListener("click", applyDeleteProfessor);
 
 loadTable();
+
+function sortTable(table, dir, n) {
+  var rows, switching, i, x, y, shouldSwitch, switchcount = 0;
+  switching = true;
+  
+  while (switching) {
+    
+    switching = false;
+    rows = table.rows;    
+    for (i = 1; i < (rows.length - 1); i++) {      
+      shouldSwitch = false;      
+      x = rows[i].getElementsByTagName("TD")[n];
+      y = rows[i + 1].getElementsByTagName("TD")[n];
+      if (dir == "asc") {
+        if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+          shouldSwitch = true;
+          break;
+        }
+      } else if (dir == "desc") {
+        if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+          shouldSwitch = true;
+          break;
+        }
+      }
+    }
+    if (shouldSwitch) {
+      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+      switching = true;
+      switchcount++;
+    } else {
+      if (switchcount == 0 && dir == "asc") {
+        dir = "desc";
+        switching = true;
+      }
+    }
+  }
+}
