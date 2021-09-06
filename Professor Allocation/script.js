e74f1ed6df4bc0f702ca00b327bc7cc55729f5ed
Maurício Junior <mauriciogen3@hotmail.com>
2021-09-06 07:51:46 -0300

    Sair

---
+++ Professor Allocation/script.js
@@ -0,0 +1,8 @@
+
+function sair() {
+	var saida = confirm("Tem certeza que deseja sair?");
+	if (saida == true) {
+		window.location.replace("https://elegant-knuth-94245c.netlify.app/");
+	} else {
+	}
+}
