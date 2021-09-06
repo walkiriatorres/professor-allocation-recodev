596c43bf35274ce2a0938c68c355b2f1039315a9
Mauriciogen3 <mauriciogen3@hotmail.com>
2021-09-06 01:25:44 -0300

    botao sair
    Acrescentei o botao sair no index de alocação , fiz a função sair no JS e finalizei estilizando botao sair adicionado a professor. Nesse passo tive que tirar o href, do index, pois ele estava dando conflito. Entao tive que colocar o cursor para poder aparecer a maozinha!

--- courses/script.js
+++ courses/script.js
@@ -41,25 +41,25 @@ async function refreshTable() {
 	loadTable();
 }
 
-async function loadTable(){
+async function loadTable() {
 	let txtSearch = document.getElementById("txtSearch").value;
 	let filter = route;
 
 	if (txtSearch) {
 		filter = route + "?partName=" + txtSearch;
-	} 
+	}
 
 	let data = await getData(filter);
-	
-	if(!data.length) {
-		document.getElementById("showNotData").hidden=false;
-		document.getElementById("table").hidden=true;
+
+	if (!data.length) {
+		document.getElementById("showNotData").hidden = false;
+		document.getElementById("table").hidden = true;
 	} else {
-		document.getElementById("showNotData").hidden=true;
-		document.getElementById("table").hidden=false;
+		document.getElementById("showNotData").hidden = true;
+		document.getElementById("table").hidden = false;
 	}
-	
-	for (let item of data){
+
+	for (let item of data) {
 		createLine(item);
 	}
 }
@@ -70,19 +70,19 @@ function clearSearch() {
 }
 
 function btnAdd_click() {
-	document.getElementById("txtName").value = "";	
+	document.getElementById("txtName").value = "";
 	const title = document.getElementById("modalCreateTitle");
 	title.textContent = "Adicionar Curso";
 	actualId = undefined;
-	
+
 	document.getElementById("btnModalCreate").disabled = true;
-	document.getElementById("txtName").addEventListener("input", function(event){
-    	var conteudo = document.getElementById("txtName").value;
-   	if (conteudo !== null && conteudo !== '') {      
-    	  document.getElementById("btnModalCreate").disabled = false;
-  	} else {
-   	   document.getElementById("btnModalCreate").disabled = true;
-   	}
+	document.getElementById("txtName").addEventListener("input", function (event) {
+		var conteudo = document.getElementById("txtName").value;
+		if (conteudo !== null && conteudo !== '') {
+			document.getElementById("btnModalCreate").disabled = false;
+		} else {
+			document.getElementById("btnModalCreate").disabled = true;
+		}
 	});
 }
 function iconUpdate_click(cour) {
@@ -91,15 +91,15 @@ function iconUpdate_click(cour) {
 
 	document.getElementById("txtName").value = cour.name;
 	actualId = cour.id;
-	
+
 	document.getElementById("btnModalCreate").disabled = true;
-	document.getElementById("txtName").addEventListener("input", function(event){
-    	var conteudo = document.getElementById("txtName").value;
-   	if (conteudo !== null && conteudo !== '') {      
-    	  document.getElementById("btnModalCreate").disabled = false;
-  	} else {
-   	   document.getElementById("btnModalCreate").disabled = true;
-   	}
+	document.getElementById("txtName").addEventListener("input", function (event) {
+		var conteudo = document.getElementById("txtName").value;
+		if (conteudo !== null && conteudo !== '') {
+			document.getElementById("btnModalCreate").disabled = false;
+		} else {
+			document.getElementById("btnModalCreate").disabled = true;
+		}
 	});
 
 	var myModal = new bootstrap.Modal(document.getElementById('modalCreate'))
@@ -108,17 +108,17 @@ function iconUpdate_click(cour) {
 }
 
 function iconDelete_click(cour) {
-	actualId = cour.id;	
+	actualId = cour.id;
 
 	const txtCourse = document.getElementById('txtDeleteCourse');
-	txtCourse.textContent = cour.name;	
-	document.getElementById('msgConfirmDelete').hidden=false;
-	document.getElementById('msgConfirmDeleteAll').hidden=true;
+	txtCourse.textContent = cour.name;
+	document.getElementById('msgConfirmDelete').hidden = false;
+	document.getElementById('msgConfirmDeleteAll').hidden = true;
 
 	var myModal = new bootstrap.Modal(document.getElementById('modalDelete'))
 	myModal.show();
 
-} 
+}
 
 async function applyAddProfessor() {
 	const name = document.getElementById("txtName").value;
@@ -130,13 +130,13 @@ async function applyAddProfessor() {
 	} else {
 		result = await update(route + actualId, { name });
 	}
-	if(result) {
+	if (result) {
 		refreshTable();
 	}
 }
 async function applyDeleteProfessor() {
 	let result
-	
+
 	if (!actualId) {
 		result = await deleteData(route);
 	} else {
@@ -163,11 +163,11 @@ function btnDeleteAll_click() {
 	actualId = undefined;
 	let txtDepartment = document.getElementById('txtDeleteCourse');
 	txtDepartment.textContent = "";
-	
+
 	const title = document.getElementById("modalDeleteTitle");
 	title.textContent = "Deletar Todos Cursos";
-	document.getElementById('msgConfirmDelete').hidden=true;
-	document.getElementById('msgConfirmDeleteAll').hidden=false;
+	document.getElementById('msgConfirmDelete').hidden = true;
+	document.getElementById('msgConfirmDeleteAll').hidden = false;
 
 	var myModal = new bootstrap.Modal(document.getElementById('modalDelete'))
 	myModal.show();
@@ -176,38 +176,46 @@ function btnDeleteAll_click() {
 loadTable();
 
 function sortTable(table, dir, n) {
-  var rows, switching, i, x, y, shouldSwitch, switchcount = 0;
-  switching = true;
-  
-  while (switching) {
-    
-    switching = false;
-    rows = table.rows;    
-    for (i = 1; i < (rows.length - 1); i++) {      
-      shouldSwitch = false;      
-      x = rows[i].getElementsByTagName("TD")[n];
-      y = rows[i + 1].getElementsByTagName("TD")[n];
-      if (dir == "asc") {
-        if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
-          shouldSwitch = true;
-          break;
-        }
-      } else if (dir == "desc") {
-        if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
-          shouldSwitch = true;
-          break;
-        }
-      }
-    }
-    if (shouldSwitch) {
-      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
-      switching = true;
-      switchcount++;
-    } else {
-      if (switchcount == 0 && dir == "asc") {
-        dir = "desc";
-        switching = true;
-      }
-    }
-  }
+	var rows, switching, i, x, y, shouldSwitch, switchcount = 0;
+	switching = true;
+
+	while (switching) {
+
+		switching = false;
+		rows = table.rows;
+		for (i = 1; i < (rows.length - 1); i++) {
+			shouldSwitch = false;
+			x = rows[i].getElementsByTagName("TD")[n];
+			y = rows[i + 1].getElementsByTagName("TD")[n];
+			if (dir == "asc") {
+				if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+					shouldSwitch = true;
+					break;
+				}
+			} else if (dir == "desc") {
+				if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+					shouldSwitch = true;
+					break;
+				}
+			}
+		}
+		if (shouldSwitch) {
+			rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+			switching = true;
+			switchcount++;
+		} else {
+			if (switchcount == 0 && dir == "asc") {
+				dir = "desc";
+				switching = true;
+			}
+		}
+	}
+}
+
+function sair() {
+	var saida = confirm("Tem certeza que deseja sair?");
+	if (saida == true) {
+		window.location.replace("https://elegant-knuth-94245c.netlify.app/");
+	} else {
+	}
 }
